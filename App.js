c392ef546f7c37b4c5120faf42bfe4a03b8cc6e0
kotchere <kotchere@verisage.us>
2019-08-13 23:51:35 +0000

    partial set up of navigation stacks

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { AppLoading } from 'expo'
+import { Root } from 'native-base'
+import { createAppContainer } from 'react-navigation'
+import NavigationServices from './src/navigation/NavigationServices'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ready: false
+    }
+    this.prevAppState = 'active'
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open to start working on your app!</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
