d2403212f2c2b58f0f6c90dd26f948d64c17a5cb
kotchere <kotchere@verisage.us>
2019-08-30 21:42:26 +0000

    login layout completed

--- src/screens/LoginScreen/LoginView.js
+++ src/screens/LoginScreen/LoginView.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import { Content, Container, Form, Input, Item } from 'native-base'
 
+import { styles } from './styles'
+import { globalStyles } from '../../common/commonStyles'
+
+const logo = require('../../../assets/icon.png')
 
  class LoginView extends Component {
    render() {
      return (
-       <View>
-         <Text>This is the LoginView</Text>  
-       </View>  
+       <Container style={globalStyles.basicContainerStyles}>
+          <Content padder
+            style={globalStyles.basicContainerStyles}
+            contentContainerStyle={styles.keyboardAwareContainer}>
+            <View style={styles.childViewOne}>
+              <Image source={logo} style={styles.imageStyles} />
+            </View>
+
+            <Form style={styles.childViewTwo}>
+               <Text style={styles.textOneStyles}>SIGN IN</Text>
+               <Item style={styles.itemStyles}>
+                 <Input style={styles.inputStyles}
+                  placeholder='Email' />
+               </Item>
+
+               <Item style={styles.itemStyles}>
+                 <Input style={styles.inputStyles}
+                  placeholder='Password' />  
+               </Item>
+
+            </Form>
+          </Content>   
+       </Container>  
      )
    }
  }
