ef2f09ac1cb6d82105a27cbb8e8c5088c7ac7f75
kotchere <kotchere@verisage.us>
2019-11-13 07:01:29 +0000

    sign up styles added

---
+++ src/screens/SignUpScreen/styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native'
+import { moderateScale, fontWeight } from '../../common/commonStyles'
+
+export const styles = StyleSheet.create({
+  keyboardAwareContainer: {
+   flexGrow: 1,
+   paddingHorizontal: '12%'
+  },  
+  childOneView: {
+    flex: 4,
+    paddingTop: '20%',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  imageStyles: {
+    // width: moderateScale(200),
+    // height: moderateScale(200),
+    // maxWidth: moderateScale(200),
+    flexDirection: 'row',
+    alignSelf: 'center'
+  },
+  childViewTwo: {
+    flex: 3,
+    justifyContent: 'center'
+  },
+  childViewThree: {
+    flex: 1,
+    paddingHorizontal: '30%',
+    justifyContent: 'center'
+  },
+  childViewFour: {
+    flex: 1
+  },
+  itemStyles: {
+    backgroundColor: 'white',
+    marginVertical: 8,
+    borderRadius: 2
+  },
+  inputStyles: {
+    paddingLeft: moderateScale(5),
+    fontSize: moderateScale(20),
+    fontWeight: '200',
+    color: 'gray'
+  },
+  textOneStyles: {
+    alignSelf: 'center',
+    fontSize: moderateScale(25),
+    fontWeight: fontWeight
+  },
+  buttonRowSectionStyles: {
+    flex: 1,
+    flexDirection: 'row',
+    // justifyContent: 'space-around',
+    alignSelf: 'center'
+  },
+  linkStyles: {
+    color:'#e93766',
+    fontSize: 18
+  }
+})
